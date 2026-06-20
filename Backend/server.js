9f5fb174a2dc32d27f13dafa7da4bb0c07fb8a47
rensodalin <rensodalin@user.noreply.github.com>
2026-06-20 18:54:03 +0700

    Modify Hosting

--- Backend/server.js
+++ Backend/server.js
@@ -65,6 +65,7 @@ app.use(session({
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    partitioned: process.env.NODE_ENV === "production",
   },
 }));
 
